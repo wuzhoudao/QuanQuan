efce5ca25f9f6d7d7dcb9e3e0302f559450e8dcc
wuzhoudao <bjwuzhoudao@BIH-D-4433.netease.internal>
2016-04-16 19:58:10 +0800

    短信验证

--- app/src/main/java/com/jack/quanquan/MainActivity.java
+++ app/src/main/java/com/jack/quanquan/MainActivity.java
@@ -1,13 +1,75 @@
 package com.jack.quanquan;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Random;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.gui.RegisterPage;
+
+
+public class MainActivity extends Activity {
+
+    //此APPKEY仅供测试使用，且不定期失效，请到mob.com后台申请正式APPKEY
+    private static String APPKEY = "11bc19667ba1a";
+
+    // 填写从短信SDK应用后台注册得到的APPSECRET
+    private static String APPSECRET = "0c44a4949a61c1c5396b4aa6dc43af0b";
+    private static final String[] AVATARS = {
+            "http://tupian.qqjay.com/u/2011/0729/e755c434c91fed9f6f73152731788cb3.jpg",
+            "http://99touxiang.com/public/upload/nvsheng/125/27-011820_433.jpg",
+            "http://img1.touxiang.cn/uploads/allimg/111029/2330264224-36.png",
+            "http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339485237265.jpg",
+            "http://diy.qqjay.com/u/files/2012/0523/f466c38e1c6c99ee2d6cd7746207a97a.jpg",
+            "http://img1.touxiang.cn/uploads/20121224/24-054837_708.jpg",
+            "http://img1.touxiang.cn/uploads/20121212/12-060125_658.jpg",
+            "http://img1.touxiang.cn/uploads/20130608/08-054059_703.jpg",
+            "http://diy.qqjay.com/u2/2013/0422/fadc08459b1ef5fc1ea6b5b8d22e44b4.jpg",
+            "http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339510584349.jpg",
+            "http://img1.touxiang.cn/uploads/20130515/15-080722_514.jpg",
+            "http://diy.qqjay.com/u2/2013/0401/4355c29b30d295b26da6f242a65bcaad.jpg"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SMSSDK.initSDK(this, APPKEY, APPSECRET);
         setContentView(R.layout.activity_main);
+        start();
+
+    }
+
+    private void start() {
+        //打开注册页面
+        RegisterPage registerPage = new RegisterPage();
+        registerPage.setRegisterCallback(new EventHandler() {
+            public void afterEvent(int event, int result, Object data) {
+                // 解析注册结果
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    @SuppressWarnings("unchecked")
+                    HashMap<String, Object> phoneMap = (HashMap<String, Object>) data;
+                    String country = (String) phoneMap.get("country");
+                    String phone = (String) phoneMap.get("phone");
+
+                    // 提交用户信息
+                    registerUser(country, phone);
+                }
+            }
+        });
+        registerPage.show(MainActivity.this);
+    }
+
+    // 提交用户信息
+    private void registerUser(String country, String phone) {
+        Random rnd = new Random();
+        int id = Math.abs(rnd.nextInt());
+        String uid = String.valueOf(id);
+        String nickName = "SmsSDK_User_" + uid;
+        String avatar = AVATARS[id % 12];
+        SMSSDK.submitUserInfo(uid, nickName, avatar, country, phone);
     }
 }
